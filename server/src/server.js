59c261ac2d3201e890ce05a2c3185de54629d8fe
jelarson <jesslarson93@gmail.com>
2023-05-02 21:42:56 -0600

    test mongoose connection

--- server/src/server.js
+++ server/src/server.js
@@ -11,6 +11,14 @@ const MONGO_URL = 'somemongourl';
 
 const server = http.createServer(app)
 
+mongoose.connection.once('open', () => {
+    console.log("MongoDB connection ready!");
+});
+
+mongoose.connection.on('error', (err) => {
+    console.error(err);
+})
+
 async function startServer() {
     await mongoose.connect(MONGO_URL, {
         useNewUrlParser: true,
