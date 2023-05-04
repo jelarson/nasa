189c71e0232ca04a1742c71e08da3a4c64d677f2
jelarson <jesslarson93@gmail.com>
2023-05-03 21:25:17 -0600

    Launches schema

---
+++ server/src/models/launches.mongo.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const launchesSchema = new mongoose.Schema({
+  flightNumber: {
+    type: Number,
+    required: true,
+    // default: 100,
+    // min: 100,
+    // max: 999,
+  },
+  launchDate: {
+    type: Date,
+    required: true,
+  },
+  mission: {
+    type: String,
+    required: true,
+  },
+  rockets: {
+    type: String,
+    required: true,
+  }
+})
