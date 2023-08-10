574e7efe33d55f74f2370d7ef73166d9d03ff32d
jelarson <jesslarson93@gmail.com>
2023-08-09 21:10:59 -0600

    ok: true

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -43,9 +43,11 @@ async function httpAbortLaunch(req, res) {
     if (!aborted) {
         return res.status(400).json({
             error: 'Launch not aborted',
-        })
+        });
     }
-    return res.status(200).json(aborted);
+    return res.status(200).json({
+        ok: true,
+    });
 }
 
 module.exports = {
