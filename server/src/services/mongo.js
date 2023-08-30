4680652d5f83c35ff5252e60e33fd985f547143f
jelarson <jesslarson93@gmail.com>
2023-08-29 21:52:46 -0600

    console log error

--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -3,3 +3,7 @@ const mongoose = require('mongoose');
 mongoose.connection.once('open', () => {
   console.log('MongoDb connection ready!');
 })
+
+mongoose.on('error', (err) => {
+  console.log(err);
+})
