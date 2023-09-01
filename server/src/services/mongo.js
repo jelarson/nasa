ef6f59474c4072da93387d22aaf788f608548ddd
jelarson <jesslarson93@gmail.com>
2023-08-31 21:39:32 -0600

    mongoConnect()

--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -9,3 +9,12 @@ mongoose.connection.once('open', () => {
 mongoose.on('error', (err) => {
   console.log(err);
 })
+
+async function mongoConnect() {
+  await mongoose.connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+});
+}
