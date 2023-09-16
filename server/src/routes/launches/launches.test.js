4b79fbc7dc0d71bf1226a8bd7c869f338f26fcda
jelarson <jesslarson93@gmail.com>
2023-09-15 23:45:58 -0600

    beforeAll mongoConnect

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -1,7 +1,12 @@
 const request = require('supertest');
 const app = require('../../app');
+const { mongoConnect } = require('../../services/mongo');
 
 describe('Launches API', () => {
+    beforeAll(async () => {
+        await mongoConnect();
+    })
+
     describe('Test GET /launches', () => {
         test('It should respond with 200 success', async () => {
             const response = await request(app)
