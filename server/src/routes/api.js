29c3f96f9c6289adc956152a5bd1cfd2ee164675
jelarson <jesslarson93@gmail.com>
2023-10-13 20:44:03 -0600

    express.Router()

--- server/src/routes/api.js
+++ server/src/routes/api.js
@@ -3,5 +3,7 @@ const express = require('express');
 const planetsRouter = require('./routes/planets/planets.router');
 const launchesRouter = require('./routes/launches/launches.router');
 
+const api = express.Router();
+
 app.use('/v1/planets', planetsRouter);
 app.use('/v1/launches', launchesRouter);
