db6b58d2ae368ce356a7ce2f1fff3f4206aa7f9c
jelarson <jesslarson93@gmail.com>
2023-10-25 16:27:41 -0600

    use(api)

--- server/src/app.js
+++ server/src/app.js
@@ -17,6 +17,8 @@ app.use(morgan('combined'))
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
+app.use(api);
+
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 })
