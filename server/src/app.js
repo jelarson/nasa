efc94daa1a8e1c293d3fcfc4f0da4e8895e98d7a
jelarson <jesslarson93@gmail.com>
2023-10-27 22:00:45 -0600

    version to mounting

--- server/src/app.js
+++ server/src/app.js
@@ -17,7 +17,7 @@ app.use(morgan('combined'))
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
-app.use(api);
+app.use('/v1', api);
 
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
