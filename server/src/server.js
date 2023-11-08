13ae44c3e6e7444b19297d6bbdc6b9a1d958cc11
jelarson <jesslarson93@gmail.com>
2023-11-08 14:30:14 -0700

    await loadLaunchData()

--- server/src/server.js
+++ server/src/server.js
@@ -13,6 +13,7 @@ const server = http.createServer(app)
 async function startServer() {
     await mongoConnect();
     await loadPlanetsData();
+    await loadLaunchData();
     
     server.listen(PORT, () => {
         console.log('Listening on port ' + PORT);
