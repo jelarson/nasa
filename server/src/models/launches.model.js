ea895a5b957ae52b7d4183f8bc38ada00b104ffa
jelarson <jesslarson93@gmail.com>
2024-04-02 20:57:11 -0600

    limit()

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -114,6 +114,7 @@ async function getAllLaunches() {
   // return Array.from(launches.values());
   return await launchesDatabase
     .find({}, { '_id': 0, '__v': 0 })
+    .limit()
 }
 
 async function saveLaunch(launch) {
