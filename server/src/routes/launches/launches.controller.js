20d4fab22c2cc5f8148014d74ba136fe1287f9eb
jelarson <jesslarson93@gmail.com>
2024-04-09 21:07:57 -0600

    page and limit

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -8,6 +8,7 @@ const {
 async function httpGetAllLaunches(req, res) {
     // for (value of launches.values()) {...}
     console.log(req.query)
+    const { page, limit } = req.query;
     return res.status(200).json(await getAllLaunches());
 }
 
