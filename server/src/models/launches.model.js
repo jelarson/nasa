e3c949fbddf89212f34c9d3762bd361b111ce6ab
jelarson <jesslarson93@gmail.com>
2024-05-14 21:36:48 -0600

    flightNumber 1 instead of -1

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -114,7 +114,7 @@ async function getAllLaunches(skip, limit) {
   // return Array.from(launches.values());
   return await launchesDatabase
     .find({}, { '_id': 0, '__v': 0 })
-    .sort({ flightNumber: -1 })
+    .sort({ flightNumber: 1 })
     .skip(skip)
     .limit(limit);
 }
