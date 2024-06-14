00a5a98656b6b6bd20aed06ccdd2cc053c8fba54
jelarson <jesslarson93@gmail.com>
2024-06-13 21:23:27 -0600

    semicolon

--- client/src/hooks/useLaunches.js
+++ client/src/hooks/useLaunches.js
@@ -55,7 +55,7 @@ function useLaunches(onSuccessSound, onAbortSound, onFailureSound) {
       onAbortSound();
     } else {
       onFailureSound();
-    }
+    };
   }, [getLaunches, onAbortSound, onFailureSound]);
 
   return {
