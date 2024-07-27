3773422fafa7a7e0830eee1beb06677366572b0e
jelarson <jesslarson93@gmail.com>
2024-07-26 22:57:46 -0600

    remove comment

--- server/src/models/launches.mongo.js
+++ server/src/models/launches.mongo.js
@@ -4,9 +4,6 @@ const launchesSchema = new mongoose.Schema({
   flightNumber: {
     type: Number,
     required: true,
-    // default: 100,
-    // min: 100,
-    // max: 999,
   },
   launchDate: {
     type: Date,
