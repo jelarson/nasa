538c6a2626bf24cad153ece227835d9924105b98
jelarson <jesslarson93@gmail.com>
2024-08-05 23:07:25 -0600

    remove comment

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -53,7 +53,6 @@ function loadPlanetsData() {
 async function getAllPlanets() {
     // return planets.find({
     //     keplerName: 'Kepler-62 f',
-    // }, '-keplerName anotherField')
     return await planets.find({}, {
         '_id': 0, '__v': 0,
     });
