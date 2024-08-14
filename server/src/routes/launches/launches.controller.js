7363bb7c37cea8327a7ca501af5c85faf8b9423e
jelarson <jesslarson93@gmail.com>
2024-08-13 22:59:45 -0600

    semicolon

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -11,7 +11,7 @@ const {
 
 async function httpGetAllLaunches(req, res) {
     const { skip, limit } = getPagination(req.query);
-    const launches = await getAllLaunches(skip, limit)
+    const launches = await getAllLaunches(skip, limit);
     return res.status(200).json(launches);
 }
 
