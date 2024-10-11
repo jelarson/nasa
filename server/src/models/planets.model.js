f778e9e0c5bf0eaf87ed2d879b3572612d5df6b4
jelarson <jesslarson93@gmail.com>
2024-10-10 21:13:29 -0600

    remove comment

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -9,8 +9,6 @@ function isHabitablePlanet(planet) {
     && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11 && planet['koi_prad'] < 1.6;
 };
 
-// promises.then((result) => {
-
 // });
 // const result = await promises;
 // console.log(result);
