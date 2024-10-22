dbec9769cf5240431069ce862e9fbefbf647645a
jelarson <jesslarson93@gmail.com>
2024-10-21 23:36:03 -0600

    remove comment

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -9,8 +9,6 @@ function isHabitablePlanet(planet) {
     && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11 && planet['koi_prad'] < 1.6;
 };
 
-// console.log(result);
-
 function loadPlanetsData() {
     return new Promise((resolve, reject) => {
         fs.createReadStream(path.join(__dirname, '..', '..', 'data', 'kepler_data.csv'))
