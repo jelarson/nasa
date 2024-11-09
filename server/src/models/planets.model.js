79f86b16a9413ed8ba12d1a0617d5c475e016ec4
jelarson <jesslarson93@gmail.com>
2024-11-08 20:07:59 -0700

    remove comment

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -18,7 +18,6 @@ function loadPlanetsData() {
         }))
         .on('data', async (data) => {
             if (isHabitablePlanet(data)) {
-                // });
                 savePlanet(data);
             }
         })
