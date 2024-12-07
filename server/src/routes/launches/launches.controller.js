09bdc273557ec59e31b52e2ab9d6d303377f9aa2
jelarson <jesslarson93@gmail.com>
2024-12-06 23:49:32 -0700

    semicolon

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -30,7 +30,7 @@ async function httpAddNewLaunch(req, res) {
     };
     await scheduleNewLaunch(launch);
     return res.status(201).json(launch);
-}
+};
 
 async function httpAbortLaunch(req, res) {
     const launchId = Number(req.params.id);
