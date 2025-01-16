e0c109a0529cbb3861f1ec2985526ce355a598cc
jelarson <jesslarson93@gmail.com>
2025-01-15 22:44:35 -0700

    remove line break

--- server/src/app.js
+++ server/src/app.js
@@ -14,7 +14,6 @@ app.use(cors({
 app.use(morgan('combined'));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
-
 app.use('/v1', api);
 
 app.get('/*', (_, res) => {
