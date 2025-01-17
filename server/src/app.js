f39fb6e7d0180dc91b8da5948b86a4db0ddba734
jelarson <jesslarson93@gmail.com>
2025-01-16 21:58:07 -0700

    remove line break

--- server/src/app.js
+++ server/src/app.js
@@ -15,7 +15,6 @@ app.use(morgan('combined'));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use('/v1', api);
-
 app.get('/*', (_, res) => {
     res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 });
