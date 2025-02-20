c613c595cd719e0f2db5781bfc962d66b5560263
jelarson <jesslarson93@gmail.com>
2025-02-19 20:58:31 -0700

    remove line break

--- server/src/server.js
+++ server/src/server.js
@@ -4,7 +4,6 @@ const app = require('./app');
 const { mongoConnect } = require('./services/mongo');
 const { loadPlanetsData } = require('./models/planets.model');
 const { loadLaunchData } = require('./models/launches.model');
-
 const PORT = process.env.PORT || 8001;
 
 const server = http.createServer(app);
