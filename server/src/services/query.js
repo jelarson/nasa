7893354a06793c549baae064bbc01d397d85253c
jelarson <jesslarson93@gmail.com>
2025-02-27 22:24:19 -0700

    remove line break

--- server/src/services/query.js
+++ server/src/services/query.js
@@ -1,6 +1,5 @@
 const DEFAULT_PAGE_NUMBER = 1;
 const DEFAULT_PAGE_LIMIT = 50;
-
 function getPagination(query) {
   const page = Math.abs(query.page) || DEFAULT_PAGE_NUMBER;
   const limit = Math.abs(query.limit) || DEFAULT_PAGE_LIMIT;
