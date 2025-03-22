6fa6adbc947cbe2339a2dfd6c34b68445e04fc69
jelarson <jesslarson93@gmail.com>
2025-03-21 20:07:10 -0600

    remove line break

--- server/src/routes/api.js
+++ server/src/routes/api.js
@@ -2,7 +2,6 @@ const express = require('express');
 const planetsRouter = require('./planets/planets.router');
 const launchesRouter = require('./launches/launches.router');
 const api = express.Router();
-
 app.use('/planets', planetsRouter);
 app.use('/launches', launchesRouter);
 
