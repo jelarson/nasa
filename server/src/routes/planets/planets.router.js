1fafa19e7192c703ef15cb79b78aa77fa81cd34d
jelarson <jesslarson93@gmail.com>
2025-03-31 18:30:12 -0600

    remove line break

--- server/src/routes/planets/planets.router.js
+++ server/src/routes/planets/planets.router.js
@@ -3,7 +3,6 @@ const {
     httpGetAllPlanets,
 } = require('./planets.controller');
 const planetsRouter = express.Router();
-
 planetsRouter.get('/', httpGetAllPlanets);
 
 module.exports = planetsRouter;
