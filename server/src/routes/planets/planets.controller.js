4c00d3c31c1c2bd4a19f3ad34136954c62f9f8f1
jelarson <jesslarson93@gmail.com>
2025-04-21 23:00:55 -0600

    remove line break

--- server/src/routes/planets/planets.controller.js
+++ server/src/routes/planets/planets.controller.js
@@ -2,7 +2,6 @@ const { getAllPlanets } = require('../../models/planets.model');
 async function httpGetAllPlanets(req, res) {
     return res.status(200).json( await getAllPlanets());
 };
-
 module.exports = {
     httpGetAllPlanets,
 };
