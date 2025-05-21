6903faff57f22d2f4c47edefa5ab58a609701337
jelarson <jesslarson93@gmail.com>
2025-05-20 21:37:31 -0600

    remove line break

--- server/src/routes/launches/launches.router.js
+++ server/src/routes/launches/launches.router.js
@@ -4,7 +4,6 @@ const {
     httpAddNewLaunch,
     httpAbortLaunch,
 } = require('./launches.controller');
-
 const launchesRouter = express.Router();
 
 launchesRouter.get('/', httpGetAllLaunches);
