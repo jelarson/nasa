9c60c628b475fc76c5ea221cfcba26eab4b11ee8
jelarson <jesslarson93@gmail.com>
2025-06-11 21:55:10 -0600

    remove line break

--- server/src/models/planets.mongo.js
+++ server/src/models/planets.mongo.js
@@ -1,10 +1,8 @@
 const mongoose = require('mongoose');
-
 const planetsSchema = new mongoose.Schema({
   keplerName: {
     type: String,
     required: true,
   }
 });
-
 module.exports = mongoose.model('Planets', planetsSchema);
