ecc6f947ecfa0de99bead7c5c5b409ed0a04e960
jelarson <jesslarson93@gmail.com>
2025-06-20 22:20:58 -0600

    remove line break

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -2,7 +2,6 @@ const fs = require('fs');
 const path = require('path');
 const { parse } = require('csv-parse');
 const planets = require('./planets.mongo');
-
 function isHabitablePlanet(planet) {
     return planet['koi_disposition'] == 'CONFIRMED'
     && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11 && planet['koi_prad'] < 1.6;
