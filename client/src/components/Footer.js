56389e6e32ce658b497e970c20330cbf5be9a2ed
Jess Larson <jesslarson93@gmail.com>
2026-04-16 21:23:35 -0600

    add space

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,6 +3,7 @@ import { Footer as ArwesFooter, Paragraph } from "arwes";
 import Centered from "./Centered";
 
 const Footer = () => {
+  
   return <ArwesFooter animate>
     <Centered>
       <Paragraph style={{ fontSize: 14, margin: "10px 0" }}>
