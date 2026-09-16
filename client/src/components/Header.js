02c8715acfb4917820bb924ef8c109693e4764ff
Jess Larson <jesslarson93@gmail.com>
2026-09-15 23:23:06 -0600

    Add line break

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -149,6 +149,7 @@ const Header = props => {
         <Clickable className={classes.clickable} onClick={onNav}>
           
           <Highlight className={classes.button} animate layer="header">
+            
             <Link className={classes.link} to="/history">
             <i className="material-icons">history</i>History</Link>
           </Highlight>
